7445067a192e2d7e531820d919dc814540607967
Siam11651 <siam11651@outlook.com>
2024-02-22 00:57:10 +0600

    kaaj kori

--- offline-3/src/main.cpp
+++ offline-3/src/main.cpp
@@ -281,7 +281,7 @@ int main(int argc, char **argv)
     uint32_t dy_loc = glGetUniformLocation(shader_program, "dy");
 
     glm::vec3 circle_colors[] = {glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)};
-    glm::vec3 centers[] = {glm::vec3(1.25f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)};
+    glm::vec3 centers[] = {glm::vec3(1.5f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)};
 
     for(size_t i = 0; i < 2; ++i)
     {
